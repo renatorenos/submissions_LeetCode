56cdbc4e43acf5e59aa17e96a06cec988ea64288
Renato Souza <renatorenos@gmail.com>
2023-09-05 16:27:14 -0300

    ADD:Easy 20. Valid Parentheses

---
+++ EASY_Valid_Parentheses.java
@@ -0,0 +1,35 @@
+package submissions_LeetCode;
+import java.util.Stack;
+
+public class EASY_Valid_Parentheses {
+    public static void main(String[] args) {
+        String[] inputs = {"()", "()[]{}", "(]", "){"};        
+        for (String s: inputs)
+            System.out.println(isValid(s)); 
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> sChar = new Stack<>();
+
+        if(s.length() == 0){
+            return true;
+        }
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '['  || c == '{' )
+                sChar.push(c);
+            else {
+                if (!sChar.isEmpty()) {
+                    if ( (c == ')' && sChar.peek() == '(') || (c == ']' && sChar.peek() == '[') || (c == '}' && sChar.peek() == '{') )
+                        sChar.pop();
+                    else 
+                        return false;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return sChar.isEmpty();
+    }
+}
