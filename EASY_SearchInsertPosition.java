871a11b79680fece472c08b7d2101f9d9479ea15
Renato <renatorenos@gmail.com>
2023-09-28 00:31:31 -0300

    modified:   EASY_SearchInsertPosition.java

--- EASY_SearchInsertPosition.java
+++ EASY_SearchInsertPosition.java
@@ -7,7 +7,22 @@ public class EASY_SearchInsertPosition {
     }
 
     public static int searchInsert(int[] nums, int target) {
-        
-        return 0;
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int current = (start + end) / 2;
+
+            if (nums[current] == target) {
+                return current;
+            }
+
+            if (nums[current] > target) {
+                end = current - 1;
+            } else {
+                start = current + 1;
+            }
+        }
+        return start;
     }
 }
